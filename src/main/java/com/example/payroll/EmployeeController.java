8d86c7dd0dc71125c4cd06058cca7d3ee8b42e6d
Qingquan Li <liqqisme@gmail.com>
2022-10-30 00:07:26 -0400

    Make more RESTful

--- src/main/java/com/example/payroll/EmployeeController.java
+++ src/main/java/com/example/payroll/EmployeeController.java
@@ -5,6 +5,8 @@
 package com.example.payroll;
 
 // import org.springframework.web.bind.annotation.*;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 
 // `@RestController` indicates that the data returned by each method will be
@@ -33,11 +39,27 @@ class EmployeeController {
 
     // Aggregate root
     // tag::get-aggregate-root[]
-    @GetMapping("/employees")
-    List<Employee> all() {
-        return repository.findAll();
-    }
+//    @GetMapping("/employees")
+//    List<Employee> all() {
+//        return repository.findAll();
+//    }
     // end::get-aggregate-root[]
+
+    /**
+     * To make the aggregate root more RESTful, you want to include top level
+     * links while ALSO including any RESTful components within.
+     */
+    @GetMapping("/employees")
+    CollectionModel<EntityModel<Employee>> all() {
+
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+                .map(employee -> EntityModel.of(employee,
+                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
+    }
 
     @PostMapping("/employees")
     Employee newEmployee(@RequestBody Employee newEmployee) {
@@ -45,12 +67,31 @@ class EmployeeController {
     }
 
     // Single item
+//    @GetMapping("/employees/{id}")
+//    Employee one(@PathVariable Long id) {
+//
+//        return repository.findById(id)
+//                .orElseThrow(() -> new EmployeeNotFoundException(id));
+//    }
 
+    /**
+     * A critical ingredient to any RESTful service is adding links to
+     * relevant operations. To make your controller more RESTful,
+     * add links like this:
+     * This decompressed output shows not only the data elements
+     * (id, name and role), but also a _links entry containing two URIs.
+     */
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id) {
+    EntityModel<Employee> one(@PathVariable Long id) {
 
-        return repository.findById(id)
+        Employee employee = repository.findById(id) //
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        return EntityModel.of(employee, //
+                linkTo(methodOn(EmployeeController.class).
+                        one(id)).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).
+                        all()).withRel("employees"));
     }
 
     /**
